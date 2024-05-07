d60d08cd96dba8de6b5c220cf80c0eff22ce013c
liuqiong <muoo.l@outlook.com>
2024-05-07 14:27:39 +0800

    修改python文件，适配空格文件路径

--- build.py
+++ build.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python
 
-
 import os
 import threading
 import sys
 
-runing = True
-
+running = True
 
 def readfile(logfile):
     with open(logfile, 'w') as f:
         pass
     with open(logfile, 'r') as f:
-        while runing:
+        while running:
             line = f.readline(1000)
             if line != '':
                 line = line.replace('\\', '/')
                 print(line, end='')
 
-
 if __name__ == '__main__':
     modulePath = os.path.abspath(os.curdir)
     logfile = modulePath + '/.vscode/build.log'
-    cmd = ''
+    cmd = ""
     for i in range(1, len(sys.argv)):
-        cmd += sys.argv[i] + ' '
-    cmd += '-j0 -o ' + logfile
+        # 如果是路径参数，确保用双引号括起来
+        if os.path.exists(sys.argv[i]):
+            cmd += '"' + sys.argv[i] + '" '
+        else:
+            cmd += sys.argv[i] + ' '
+    cmd += '-j0 -o ' + '"' + logfile + '"'
     print(cmd)
     thread = threading.Thread(target=readfile, args=(logfile,))
     thread.start()
-    code = os.system(cmd)
-    runing = False
+    # 使用subprocess.run代替os.system增加安全性和控制性
+    import subprocess
+    result = subprocess.run(cmd, shell=True, text=True, capture_output=True)
+    print(result.stdout)
+    running = False
     thread.join()
-    sys.exit(code)
+    if result.returncode == 0:
+        sys.exit(0)
+    else:
+        sys.exit(result.returncode)
